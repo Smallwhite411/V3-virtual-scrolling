0abc7dfb9882517df11fce6acff201226fa62a37
邢晓龙 <1056325082@qq.com>
2025-08-14 17:04:12 +0800

    pref：不定高虚拟列表组件优化

--- src/components/TextVaribleHeightVirtual/index.vue
+++ src/components/TextVaribleHeightVirtual/index.vue
@@ -5,7 +5,10 @@
     class="viewport"
     @scroll="handleScroll"
   >
-    <div class="content-placeholder"></div>
+    <div
+      class="content-placeholder"
+      :style="{ height: `${renderTotalHeight}px` }"
+    ></div>
     <div class="content-wrap" :style="{ transform: `translateY(${offset}px)` }">
       <div
         v-for="item in renderItems"
@@ -20,7 +23,7 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, nextTick, computed } from "vue";
 const props = defineProps({
   allItems: {
     type: Array,
@@ -43,9 +46,16 @@ const props = defineProps({
     default: 5,
   },
 });
+const cacheHeight = new Map(); // 缓存每个 item 的高度
+const minItemHeight = ref(null);
+const renderTotalHeight = computed(() => {
+  console.log("asjfboea", minItemHeight.value);
+  if (minItemHeight.value === null)
+    return 0; // 如果没有测量过任何 item 的高度，返回 0
+  else return minItemHeight.value * props.allItems.length;
+});
 const viewportRef = ref(null);
 const renderItems = ref([]); // 当前需要渲染的 item
-const hasRenderedItemsHeight = ref({}); // 已渲染的 item 数据 height
 const offset = ref(0);
 // 更新已渲染的 item 高度
 const updateRenderItems = () => {
@@ -53,9 +63,8 @@ const updateRenderItems = () => {
 
   let startIndex = 0;
   let startOffset = 0;
-
   for (let i = 0; i < props.allItems.length; i++) {
-    const h = hasRenderedItemsHeight.value[props.allItems[i][props.keyField]];
+    const h = cacheHeight.get(props.allItems[i][props.keyField]);
     startOffset += h;
     if (startOffset >= scrollTop) {
       startIndex = i;
@@ -68,19 +77,24 @@ const updateRenderItems = () => {
     startIndex + props.renderSize + props.buffer
   );
   offset.value =
-    startOffset - hasRenderedItemsHeight.value[props.allItems[startIndex][props.keyField]];
+    startOffset - cacheHeight.get(props.allItems[startIndex][props.keyField]);
 };
 
 const renderItemsRef = (el, id) => {
   if (el) {
     // 存放已渲染的 item 的高度
-    hasRenderedItemsHeight.value[id] = el.offsetHeight;
+    if (!cacheHeight.has(id)) {
+      // 计算并缓存该 item 的高度
+      cacheHeight.set(id, el.offsetHeight);
+      if (minItemHeight.value === null) minItemHeight.value = el.offsetHeight;
+      // 更新最小高度
+      else minItemHeight.value = Math.min(minItemHeight.value, el.offsetHeight);
+    }
   }
 };
 const handleScroll = () => {
   updateRenderItems();
 };
-
 onMounted(() => {
   updateRenderItems();
 });
@@ -97,7 +111,6 @@ onMounted(() => {
   top: 0;
   right: 0;
 }
-
 .content-item {
   border: 1px solid #000;
   display: flex;
